57d1309a4d7b9ab24420d10ea9e3ac21062fd202
Yadav Kumar <125194075+RaoSuren@users.noreply.github.com>
2023-09-10 15:58:30 +0000

    Update 5 files

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,61 +1,38 @@
-const AudioCall = require("../models/audioCall");
-const FriendRequest = require("../models/friendRequest");
-const User = require("../models/user");
-const VideoCall = require("../models/videoCall");
-const catchAsync = require("../utils/catchAsync");
-const filterObj = require("../utils/filterObj");
+const router = require("express").Router();
 
-const { generateToken04 } = require("./zegoServerAssistant");
+const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
 
-// Please change appID to your appId, appid is a number
-// Example: 1234567890
-const appID = process.env.ZEGO_APP_ID; // type: number
+router.post(
+  "/generate-zego-token",
+  authController.protect,
+  userController.generateZegoToken
+);
+router.get(
+  "/get-call-logs",
+  authController.protect,
+  userController.getCallLogs
+);
+router.get("/get-me", authController.protect, userController.getMe);
+router.patch("/update-me", authController.protect, userController.updateMe);
+router.get(
+  "/get-all-verified-users",
+  authController.protect,
+  userController.getAllVerifiedUsers
+);
+router.get("/get-users", authController.protect, userController.getUsers);
+router.get("/get-requests", authController.protect, userController.getRequests);
+router.get("/get-friends", authController.protect, userController.getFriends);
 
-// Please change serverSecret to your serverSecret, serverSecret is string
-// Example：'sdfsdfsd323sdfsdf'
-const serverSecret = process.env.ZEGO_SERVER_SECRET; // type: 32 byte length string
+router.post(
+  "/start-audio-call",
+  authController.protect,
+  userController.startAudioCall
+);
+router.post(
+  "/start-video-call",
+  authController.protect,
+  userController.startVideoCall
+);
 
-exports.getMe = catchAsync(async (req, res, next) => {
-  res.status(200).json({
-    status: "success",
-    data: req.user,
-  });
-});
-
-exports.updateMe = catchAsync(async (req, res, next) => {
-  const filteredBody = filterObj(
-    req.body,
-    "firstName",
-    "lastName",
-    "about",
-    "avatar"
-  );
-
-  const userDoc = await User.findByIdAndUpdate(req.user._id, filteredBody);
-
-  res.status(200).json({
-    status: "success",
-    data: userDoc,
-    message: "User Updated successfully",
-  });
-});
-
-exports.getUsers = catchAsync(async (req, res, next) => {
-  const all_users = await User.find({
-    verified: true,
-  }).select("firstName lastName _id");
-
-  const this_user = req.user;
-
-  const remaining_users = all_users.filter(
-    (user) =>
-      !this_user.friends.includes(user._id) &&
-      user._id.toString() !== req.user._id.toString()
-  );
-
-  res.status(200).json({
-    status: "success",
-    data: remaining_users,
-    message: "Users found successfully!",
-  });
-});
+module.exports = router;
